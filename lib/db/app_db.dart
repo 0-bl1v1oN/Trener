d8bbf0be66720c63274fa0f46a011f914214b1d2
maksbespalov00-dotcom <maks_bespalov_00@mail.ru>
2026-02-16 22:42:25 +0300

    Update app_db.dart

--- lib/db/app_db.dart
+++ lib/db/app_db.dart
@@ -378,7 +378,7 @@ class AppDb extends _$AppDb {
     }
 
     final q = customSelect(
-      "SELECT date(datetime(a.${appointments.startAt.name} / 1000, 'unixepoch', 'localtime')) AS d, COUNT(*) AS c "
+      "SELECT date(datetime(CASE WHEN a.${appointments.startAt.name} > 20000000000 THEN a.${appointments.startAt.name} / 1000 ELSE a.${appointments.startAt.name} END, 'unixepoch', 'localtime')) AS d, COUNT(*) AS c "
       'FROM ${appointments.actualTableName} a '
       'INNER JOIN ${clients.actualTableName} c '
       'ON c.${clients.id.name} = a.${appointments.clientId.name} '
@@ -407,7 +407,7 @@ class AppDb extends _$AppDb {
     required DateTime to,
   }) {
     final q = customSelect(
-      "SELECT date(datetime(${clients.planEnd.name} / 1000, 'unixepoch', 'localtime')) AS d, COUNT(*) AS c "
+      "SELECT date(datetime(CASE WHEN ${clients.planEnd.name} > 20000000000 THEN ${clients.planEnd.name} / 1000 ELSE ${clients.planEnd.name} END, 'unixepoch', 'localtime')) AS d, COUNT(*) AS c "
       'FROM ${clients.actualTableName} '
       'WHERE ${clients.planEnd.name} IS NOT NULL '
       'AND ${clients.planEnd.name} >= ? AND ${clients.planEnd.name} < ? '
